e6f6a389af8ae698837bda48d26600be9adb7d71
wabisachin <wabisachin1122@yahoo.co.jp>
2019-09-05 02:45:12 +0000

    マイページの実装、編集update機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
+    
     def show
+        @user = User.find(current_user.id)
+    end
+    
+    def edit
+        @user = User.find(params[:id])
+    end
+    
+    def update
+        user = User.find(params[:id])
+        user.update(params_user)
+        redirect_to user_path(user)
+    end
+    
+    # ActiveStorageのストロングパラメータで:imageを追加すること
+    private
+    def params_user
+        params.require(:user).permit(:name,:image,:email)
     end
 end
